2a262406ec1a98783228620f25afe64a75651aa8
Joel Silva <joelcsilva@outlook.com>
2019-12-24 00:19:11 -0300

    componente menu

---
+++ src/components/menu/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.css";
+
+export default function Menu() {
+  return (
+    <>
+      <div className="menu">
+        <ul>
+          <li>
+            <button>home</button>
+          </li>
+          <li>
+            <button className="active">faq</button>
+          </li>
+          <li>
+            <button>locais</button>
+          </li>
+          <li>
+            <button>categorias</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
